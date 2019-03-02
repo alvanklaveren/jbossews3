d368db7610f4bdea3b9a06c28327fe27c647ddb1
alvanklaveren <alvanklaveren@gmail.com>
2019-03-02 15:34:54 +0100

    fixed me.jpg

--- src/main/java/com/myforum/base/BasePanelLeft.java
+++ src/main/java/com/myforum/base/BasePanelLeft.java
@@ -2,7 +2,6 @@ package com.myforum.base;
 
 import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.request.resource.ContextRelativeResource;
 
 public class BasePanelLeft extends Panel{
 	private static final long serialVersionUID = 1L;
@@ -11,8 +10,6 @@ public class BasePanelLeft extends Panel{
 	
 	public BasePanelLeft(String id){
 		super(id);	
-		meImage = new Image("me", new ContextRelativeResource("images/me.jpg"));
-		add( meImage );
 	}
 
 }
