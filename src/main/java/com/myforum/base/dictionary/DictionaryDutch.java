d172d9073d2f2db5fe3d699a6a5bd0fcba1c2d39
alvanklaveren <alvanklaveren@gmail.com>
2019-03-26 19:21:03 +0100

    few minor textual updates

--- src/main/java/com/myforum/base/dictionary/DictionaryDutch.java
+++ src/main/java/com/myforum/base/dictionary/DictionaryDutch.java
@@ -80,7 +80,7 @@ public final class DictionaryDutch extends Dictionary {
 		put(translatedSentenceMap, EText.ABOUT_WEBSITE, "Over ...");
 
 		StringBuilder aboutText = new StringBuilder();
-		aboutText.append( "Alvanklaveren.com is gemaakt, geschreven en wordt onderhouden door  A.L. van Klaveren. Momenteel is het gebouwd in " ); 
+		aboutText.append( "Deze website is gemaakt, geschreven en wordt onderhouden door  A.L. van Klaveren. Momenteel is het gebouwd in " ); 
 		aboutText.append( "Java (EE), gebruikmakend van <a href=\"http://hibernate.org/\">Apache Hibernate</a>, <a href=\"http://wicket.apache.org/\">Apache Wicket</a>, " );
 		aboutText.append( "en <a href=\"https://spring.io/\">Spring Framework</a>. De website draait op <a href=\"http://tomcat.apache.org/\">Tomcat 8</a> " );
 		aboutText.append( "op een hosting platform genaamd <a href = \"https://www.digitalocean.com/\">Digital Ocean</a>. Verder wordt de layout van de website " ); 
--- src/main/java/com/myforum/base/dictionary/DictionaryEnglish.java
+++ src/main/java/com/myforum/base/dictionary/DictionaryEnglish.java
@@ -8,7 +8,7 @@ public final class DictionaryEnglish extends Dictionary{
 		translatedWordMap.put("accessoires", "accessories"); // accidently used the dutch/french variant. Translate to english !!
 
 		StringBuilder aboutText = new StringBuilder();
-		aboutText.append( "Alvanklaveren.com is created, written and maintained by A.L. van Klaveren. It is (currently) built " );  
+		aboutText.append( "This website is created, written and maintained by A.L. van Klaveren. It is (currently) built " );  
 		aboutText.append( "in Java (EE), using <a href=\"http://hibernate.org/\">Apache Hibernate</a>, <a href=\"http://wicket.apache.org/\">Apache Wicket</a> " );
 		aboutText.append( "and <a href=\"https://spring.io/\">Spring Framework</a>. The website is running on <a href=\"http://tomcat.apache.org/\">Tomcat 8</a> " );
 		aboutText.append( "on a hosting platform called <a href = \"https://www.digitalocean.com/\">Digital Ocean</a>. Last but not least, the website's layout is " );
