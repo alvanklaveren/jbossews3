9c8d5e7544bea9b8be635ef47fc3ae01d6a7c0c3
alvanklaveren <alvanklaveren@gmail.com>
2019-03-31 19:24:18 +0200

    Fixed CV download (auto display is not alway supported AND unsafe!)

--- src/main/java/com/myforum/homepage/AboutMePage.java
+++ src/main/java/com/myforum/homepage/AboutMePage.java
@@ -1,8 +1,10 @@
 package com.myforum.homepage;
 
+import java.io.File;
 import java.util.Date;
 
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.DownloadLink;
 
 import com.myforum.application.ForumUtils;
 import com.myforum.base.AVKPage;
@@ -16,6 +18,11 @@ public class AboutMePage extends AVKPage {
 		super(EMenuItem.AboutMe);
 		
 		add( new AVKLabel("aboutme", "About Me") );
+		
+		
+		File file = ForumUtils.getFileResource("documents","cv.pdf");
+		DownloadLink downloadlink = new DownloadLink("cv", file, "cv.pdf");
+		add(downloadlink);
 		
 		Date startDate	= ForumUtils.createDate("01-04-2006", "dd-MM-yyyy"); // The day I started working at Maccs
 		int workingYears = ForumUtils.yearsBetween(startDate, ForumUtils.todayNow());
