4bd16c5c2393614499b6a417d7aa99b5e740fd97
alvanklaveren <alvanklaveren@gmail.com>
2019-04-05 19:03:55 +0200

    HTTPS

--- src/main/java/com/myforum/framework/ModalPage.java
+++ src/main/java/com/myforum/framework/ModalPage.java
@@ -3,15 +3,14 @@ package com.myforum.framework;
 import org.apache.wicket.PageReference;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
 import org.apache.wicket.markup.html.WebPage;
-//import org.apache.wicket.protocol.https.RequireHttps;
+import org.apache.wicket.protocol.https.RequireHttps;
 
-import com.myforum.base.AVKPage;
 import com.myforum.base.BasePage;
 import com.myforum.base.dictionary.EText;
 import com.myforum.base.dictionary.Translator;
 
 
-//@RequireHttps
+@RequireHttps
 public class ModalPage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
