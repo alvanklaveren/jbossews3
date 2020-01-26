12679a3f61c8aeffad593edda2e3609c0c44809c
avk <alvanklaveren@gmaill.com>
2020-01-26 14:15:13 +0100

    fixed background for <code> in darktheme

--- src/main/java/com/myforum/application/ETheme.java
+++ src/main/java/com/myforum/application/ETheme.java
@@ -3,7 +3,7 @@ package com.myforum.application;
 public enum ETheme {
 
 	  Light(1, "Light", "body{ background:#F3F3FC; color:#303030; } #centerpanel{background:#FFFFFF;} ")
-	, Dark(2, "Dark", "body{ background:#000000; color:#F3F3FC } #centerpanel{ background:#303030; color:#F3F3FC } code{color:#F3F3FC} pre{color:#F3F3FC}")
+	, Dark(2, "Dark", "body{ background:#000000; color:#F3F3FC } #centerpanel{ background:#303030; color:#F3F3FC } code{ background:#404040 } pre{background:#303030; color:#F3F3FC}")
   ;
 
 	int		id;
